3f43c4034b6485f694c16f3611029dd9a2f54a99
walid barakat <walidmohamedbarakat@gmail.com>
2024-01-27 16:11:12 +0200

    initail commit

---
+++ android/app/src/main/kotlin/com/example/save_favorite_posts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.save_favorite_posts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:save_favorite_posts/presentation/ui/home_view/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Save Favorite Posts',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
